290c27d5c24aa0ca29f65d6c18dbe48c91a61fc0
Chad Hinds <16522246+chadhindsight@users.noreply.github.com>
2025-03-26 17:10:20 +0000

    Add info for the team members

---
+++ static/script.js
@@ -0,0 +1,28 @@
+const teamInfo = [
+    {
+        name: "Luigi",
+        role: "Electronics",
+        grade: 10,
+        hobbies: ["Soccer", "Playing the trumpet", "Video Games"]
+    },
+    {
+        name: "John Cena",
+        role: "3D modeling",
+        grade: 11,
+        hobbies: ["Soccer", "Playing the trumpet", "Video Games"]
+    },
+    {
+        name: "Bianca Belair",
+        role: "Coder",
+        grade: 12,
+        hobbies: ["Soccer", "Playing the trumpet", "Video Games"]
+    },
+    {
+        name: "Mario",
+        role: "Electronics",
+        grade: 10,
+        hobbies: ["Soccer", "Playing the trumpet", "Video Games"]
+    }
+]
+
+// 
