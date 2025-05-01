c74737f51f8b7096cfcc8ca0279cfc591ee25282
Chad Hinds <16522246+chadhindsight@users.noreply.github.com>
2025-05-01 17:01:37 +0000

    Fix navbar stylings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 // ROUTING IMPORTS
 import React from 'react';
-import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
